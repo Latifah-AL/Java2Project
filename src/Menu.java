cace2ca947d4ae7cad55775ea3f24575f865dc18
Latifah Almousa <latifah@Latifahs-MacBook-Air.local>
2025-02-20 19:27:48 +0300

    Initial commit

---
+++ src/Menu.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author latifah
+ */
+public class Menu {
+    
+}
